98569358128030c258161bf212e998cfea419e76
suleyman <gameclub2015@mail.ru>
2023-01-15 12:47:22 +0500

    MVC

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const fs = require("fs");
+const methodOverride = require("method-override");
 const ejs = require("ejs");
-const path = require("path");
 const fileUpload = require("express-fileupload");
-const Photo = require("./models/Photo");
 
+
+// ! Controllers
+const photoController = require("./controllers/photoControllers");
+const pageController = require("./controllers/pageController");
 const app = express();
 
 // connect db
@@ -23,51 +25,18 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
+app.use(methodOverride("_method", { methods: ["POST", "GET"] }));
 
 //ROUTES
-app.get("/", async (req, res) => {
-  const photos = await Photo.find({}).sort('-dateCreated');
-  res.render("index", {
-    photos,
-  });
-});
+app.get("/", photoController.getAllPhotos);
+app.get("/photos/:id", photoController.getPhoto);
+app.post("/photos", photoController.createPhoto);
+app.put("/photos/:id", photoController.updatePhoto);
+app.delete("/photos/:id", photoController.deletePhoto);
 
-app.post("/photos", async (req, res) => {
-  // await Photo.create(req.body);
-  // res.redirect("/");
-
-  const uploadDir = "public/uploads";
-
-  if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
-  }
-
-  let uploadeImage = req.files.image;
-  let uploadPath = __dirname + "/public/uploads/" + uploadeImage.name;
-
-  uploadeImage.mv(uploadPath, async () => {
-    await Photo.create({
-      ...req.body,
-      image: "/uploads/" + uploadeImage.name,
-    });
-    res.redirect("/");
-  });
-});
-
-app.get("/photos/:id", async (req, res) => {
-  // console.log(req.params.id);   // https://www.tutorialspoint.com/req-params-property-in-express-js
-  const photo = await Photo.findById(req.params.id);
-  res.render("photo", {
-    photo,
-  });
-});
-
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-app.get("/add", (req, res) => {
-  res.render("add");
-});
+app.get("/photos/edit/:id", pageController.getEditPage);
+app.get("/about", pageController.getAboutPage);
+app.get("/add", pageController.getAddPage);
 
 const port = 3000;
 app.listen(port, () => {
